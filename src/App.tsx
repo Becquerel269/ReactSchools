94b3e6a7dd36e8c2e3b8b07fd56f1a3c6809be12
Rob Bracegirdle <rob269@hotmail.co.uk>
2022-03-20 12:23:33 +0000

    first location

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import './App.css';
 import { SimpleMap } from './simplemap';
 const locations = require("./locations.json");
 
+const location = {
+  address: '1600 Amphitheatre Parkway, Mountain View, california.',
+  lat: 37.42216,
+  lng: -122.08427,
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <SimpleMap name={"jude"} />
+      <SimpleMap location={location} />
       </header>
     </div>
   );
--- src/locationpin.tsx
+++ src/locationpin.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-export const SimpleMap = (props:any) => {
+export const LocationPin = (props:any) => {
     return <>
     <h1>hi rob</h1>
     <h1>{props.location.lat}</h1>
