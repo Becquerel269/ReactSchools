1b9d214becab8d352134c4e323922412a07b167c
Rob Bracegirdle <rob269@hotmail.co.uk>
2022-03-20 12:01:46 +0000

    added png

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { SimpleMap } from './simplemap';
+const locations = require("./locations.json");
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <SimpleMap name={"jude"} />
       </header>
     </div>
   );
---
+++ src/simplemap.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+export const SimpleMap = (props:any) => {
+    return <>
+    <h1>hi rob</h1>
+    <h1>{props.name}</h1>
+    </>
+ }
